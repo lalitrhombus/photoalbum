15b2ff8e1013f018a3e8fcef5baa371592344b33
Lalit Chaturvedi <lalitc@babajob.com>
2016-06-07 14:30:41 +0530

    adding routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore,combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
-require('../style/style.scss');
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
+require('../style/style.scss');
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
+
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+
+
+ReactDOM.render(
+  <Provider store={applyMiddleware(reduxPromise)(store)(reducers)}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.app'));
